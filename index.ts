5235f3340ed2901198cad138da58fe5fc57fb05f
Erica Pisani <pisani.erica@gmail.com>
2019-01-13 21:54:40 -0500

    Remove commented out code now that this is working

--- index.ts
+++ index.ts
@@ -3,10 +3,7 @@
 import "reflect-metadata";
 
 import { buildSchema } from 'type-graphql'
-// const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServer } = require('apollo-server');
-// import { prisma } from './generated/prisma-client';
-// import { IBook } from './types';
 import { BookResolver } from './types'
 
 const resolvers = [BookResolver]
@@ -27,28 +24,3 @@ async function startServer() {
 }
 
 startServer();
-// const typeDefs = gql`
-//   type Book {
-//     id: ID!
-//     title: String
-//     author: String
-//   }
-
-//   type Query {
-//     books: [Book]
-//     book(id: ID!): Book
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     books: (): Promise<Array<IBook>> => {
-//       return prisma.books();
-//     },
-//     book: (_: any, args: any): Promise<IBook> => {
-//       return prisma.book({ id: args.id })
-//     }
-//   }
-// };
-
-// const server = new ApolloServer({ typeDefs, resolvers });
